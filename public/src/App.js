3fabf55a972f8f0cf155c06309546b67c0cd6ffa
oznerol <lorenzo.ghidoni92@gmail.com>
2022-04-18 18:21:14 +0100

    fixes

--- public/src/App.js
+++ public/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react"
+import socketIOClient from "socket.io-client"
+import './App.css'
+const ENDPOINT = "http://localhost:5001"
+const socket = socketIOClient(ENDPOINT)
 
 function App() {
+  const [response, setResponse] = useState("")
+  const [message, setMessage] = useState("")
+  const [chat, setChat] = useState([])
+
+  useEffect(() => {
+    socket.on("FromAPI", data => {
+      setResponse(data)
+    })
+
+    socket.on("message", ({sentBy, text}) => {
+      setChat([...chat, {sentBy, text}])
+    })
+
+  }, [chat])
+
+  const handleChange = (e) => {
+    setMessage(e.target.value)
+  }
+
+  const sendMessage = () => {
+    socket.emit("message", {sentBy: Math.round(Math.random(0,10)*10, 1), text: message})
+    setMessage('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <p>
+        It's <time dateTime={response}>{response}</time>
+      </p>
+      <span>
+        <input
+          onChange={handleChange}
+          value={message}
+        ></input>
+        <button
+          onClick={sendMessage}
         >
-          Learn React
-        </a>
-      </header>
+          Send
+        </button>
+      </span><br/>
+      <span>
+        {chat.length !== 0 ? chat.map((msg, index) => (
+          <p key={index}>
+            {msg.sentBy}: {msg.text}
+          </p>
+        )) : <p>Send a message</p>}
+      </span>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
